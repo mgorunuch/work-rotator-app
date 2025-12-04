3e646622d536841c7e2a1659b4386459b5c1d4e5
Gerasimovich Igor <mgorunuch.igor@gmail.com>
2025-12-04 00:18:18 +0000

    ✨ Rename app to Rotator (capitalized)

--- homebrew/rotator.rb
+++ homebrew/rotator.rb
@@ -5,14 +5,14 @@ cask "rotator" do
   sha256 arm:   "REPLACE_WITH_ARM_SHA256",
          intel: "REPLACE_WITH_INTEL_SHA256"
 
-  url "https://github.com/mgorunuch/work-rotator-app/releases/download/v#{version}/rotator_#{version}_#{arch}.dmg"
+  url "https://github.com/mgorunuch/work-rotator-app/releases/download/v#{version}/Rotator_#{version}_#{arch}.dmg"
   name "Rotator"
   desc "Work rotation timer app"
   homepage "https://github.com/mgorunuch/work-rotator-app"
 
   depends_on macos: ">= :catalina"
 
-  app "rotator.app"
+  app "Rotator.app"
 
   zap trash: [
     "~/Library/Application Support/com.rotator.app",
